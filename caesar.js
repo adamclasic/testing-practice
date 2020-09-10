5e45471a39c30ea667515f914ff8e8e5375916e8
adam allalou <aabou9@gmail.com>
2020-09-10 08:46:34 +0100

    delete dycrypt

--- caesar.js
+++ caesar.js
@@ -19,17 +19,6 @@ const encrypt = (arr, ratio) => {
   return arr;
 }
 
-const decrypt = (arr, ratio) => {
-  arr.forEach((charNum, index, theArr) => {
-    if ((charNum>= 65 && charNum<= 90) || (charNum >= 97 && charNum <= 122)){
-      theArr[index] = charNum + ratio;
-    } else if (charNum>= 33 || charNum<= 63) {
-      theArr[index] = charNum + ratio;
-    }
-  });
-  return arr;
-}
-
 const charizer = (numArr) => {
   let arr = [];
   numArr.forEach(ele => {
