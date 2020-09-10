c4af3525a230d6bb0fd4cf9cf3b91dea9502fc23
adam allalou <aabou9@gmail.com>
2020-09-10 12:58:25 +0100

    update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,4 +1,5 @@
 const reverseString = function reverseString(str) {
+  if (typeof str !== 'string') {return 'the input is not a string'};
   const splitString = str.split('').reverse().join('');
   return splitString;
 };
--- reverseString.test.js
+++ reverseString.test.js
@@ -3,4 +3,6 @@ const reverseString = require('./reverseString');
 
 test('should reverse a string', () => {
   expect(reverseString('hello')).toBe('olleh');
+  expect(reverseString('dsh5gje37;]f')).toBe('f];73ejg5hsd');
+  expect(reverseString(555)).toBe('the input is not a string');
 });
